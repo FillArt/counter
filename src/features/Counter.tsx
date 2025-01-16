095fc15725a383672c2f4304facd7817d5ec2dd4
FillArt <artiom.filipopovschii@gmail.com>
2025-01-15 22:12:10 -0400

    feat: Base layout app.

--- src/features/Counter.tsx
+++ src/features/Counter.tsx
@@ -4,13 +4,21 @@ import {Button} from "../components/base/Button";
 
 export const Counter = () => {
     return (
-        <div>
-            <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', maxWidth: '200px' }}>
-                <Input placeholder={'Set start value'} />
-                <Input placeholder={'Set max value'} />
+        <div style={{display: "flex", justifyContent: "space-around", margin: "0 auto", alignItems: "center", height: "100vh", maxWidth: '400px'}}>
+            <div style={{display: 'flex', flexDirection: 'column', gap: '10px', maxWidth: '200px'}}>
+                <Input placeholder={'Set start value'}/>
+                <Input placeholder={'Set max value'}/>
                 <Button label={'Set value'} onClick={() => alert('Set values')}/>
             </div>
 
+            <div style={{display: 'flex', flexDirection: 'column', gap: '10px', textAlign: "center", maxWidth: '200px'}}>
+                <h1 style={{margin: '0px'}}>0</h1>
+
+                <div style={{display: 'flex', gap: '10px', maxWidth: '200px'}}>
+                    <Button label={'Inc'} onClick={() => alert('Inc++')}/>
+                    <Button label={'Reset'} onClick={() => alert('Reset')}/>
+                </div>
+            </div>
         </div>
     );
 };
